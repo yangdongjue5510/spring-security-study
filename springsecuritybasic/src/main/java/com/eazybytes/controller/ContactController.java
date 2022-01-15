226b7e18ff080184ff84c6ac8d4dbcf1ebe4bcb5
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-15 18:42:15 +0900

    feat: basic setting controllers

---
+++ springsecuritybasic/src/main/java/com/eazybytes/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+	@GetMapping("/contact")
+	public String saveContactInquiryDetails(String input) {
+		return "contact!";
+	}
+}
