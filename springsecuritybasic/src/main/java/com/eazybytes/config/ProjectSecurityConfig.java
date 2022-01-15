d26d6acff37a8b82679445c118e77c46a6b1ec32
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-15 21:55:49 +0900

    in memory authentication

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,30 +1,43 @@
 package com.eazybytes.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 public class ProjectSecurityConfig  extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// http.authorizeRequests()
-		// 	.antMatchers("/myAccount").authenticated()
-		// 	.antMatchers("/myBalance").authenticated()
-		// 	.antMatchers("/myLoans").authenticated()
-		// 	.antMatchers("/myCards").authenticated()
-		// 	.antMatchers("/notices").permitAll()
-		// 	.antMatchers("/contact").permitAll()
-		// 	.and()
-		// 	.formLogin()
-		// 	.and()
-		// 	.httpBasic();
+		http.authorizeRequests()
+			.antMatchers("/myAccount").authenticated()
+			.antMatchers("/myBalance").authenticated()
+			.antMatchers("/myLoans").authenticated()
+			.antMatchers("/myCards").authenticated()
+			.antMatchers("/notices").permitAll()
+			.antMatchers("/contact").permitAll()
+			.and()
+			.formLogin()
+			.and()
+			.httpBasic();
 
 		//DenyAll
 		//http.authorizeRequests().anyRequest().denyAll().and().formLogin().and().httpBasic();
 
-		http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+		//permitAll
+		//http.authorizeRequests().anyRequest().permitAll().and().formLogin().and().httpBasic();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("admin").password("1234").authorities("admin")
+			.and()
+			.withUser("user").password("5678").authorities("reader")
+			.and()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance()); //no password Encode
 	}
 }
