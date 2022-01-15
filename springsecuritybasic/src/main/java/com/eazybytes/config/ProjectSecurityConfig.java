305b611b4b8a1e16f2b7b872f8bb199de5e2e6b7
yangdongjue5510 <djwhy5510@gmail.com>
2022-01-16 00:11:34 +0900

    InMemoryUserDetailsManager

--- springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ springsecuritybasic/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.eazybytes.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig  extends WebSecurityConfigurerAdapter {
@@ -33,11 +38,23 @@ public class ProjectSecurityConfig  extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-			.withUser("admin").password("1234").authorities("admin")
-			.and()
-			.withUser("user").password("5678").authorities("reader")
-			.and()
-			.passwordEncoder(NoOpPasswordEncoder.getInstance()); //no password Encode
+		// auth.inMemoryAuthentication()
+		// 	.withUser("admin").password("1234").authorities("admin")
+		// 	.and()
+		// 	.withUser("user").password("5678").authorities("reader")
+		// 	.and()
+		// 	.passwordEncoder(NoOpPasswordEncoder.getInstance()); //no password Encode
+
+		InMemoryUserDetailsManager userDetailsService = new InMemoryUserDetailsManager();
+		UserDetails user = User.withUsername("admin").password("1234").authorities("admin").build();
+		UserDetails user1 = User.withUsername("user").password("5678").authorities("reader").build();
+		userDetailsService.createUser(user);
+		userDetailsService.createUser(user1);
+		auth.userDetailsService(userDetailsService);
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
